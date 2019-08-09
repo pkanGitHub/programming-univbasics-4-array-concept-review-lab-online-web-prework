519a82de24183ce2e0925d3e984d5d2c8f707e01
Phyllis Kan <pyk9q7@gmail.com>
2019-08-09 13:08:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,17 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  array.index(value_to_find)
+  end
 end
 
 def find_max_value(array)
   # Add your solution here
+  array.sort!
+  array[-1]
 end
 
 def find_min_value(array)
   # Add your solution here
+  array.sort!
+  array[0]
 end
